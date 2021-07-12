de047b0f5f4e6f7126eb67f00c7b4e7b7aebdbe9
Vahe1209 <Vahe1209@gmail.com>
2021-07-12 16:25:45 +0400

    feat(app.js, app.css):add new function for weather

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import sunny from "./assets/images/01.svg";
+import rain from "./assets/images/09.svg";
+import warm from "./assets/images/17.svg";
 
-function App() {
+const weatherInfo = [
+  {
+    weekDey: "Monday",
+    imgURL: sunny,
+    temp: "36C",
+  },
+  {
+    weekDey: "Tuesday",
+    imgURL: warm,
+    temp: "32C",
+  },
+  {
+    weekDey: "Wednesday",
+    imgURL: rain,
+    temp: "35C",
+  },
+  {
+    weekDey: "Thursday",
+    imgURL: rain,
+    temp: "45C",
+  },
+  {
+    weekDey: "Friday",
+    imgURL: rain,
+    temp: "36C",
+  },
+  {
+    weekDey: "Saturday",
+    imgURL: warm,
+    temp: "37C",
+  },
+  {
+    weekDey: "Sunday",
+    imgURL: sunny,
+    temp: "39C",
+  },
+];
+
+const newArr = weatherInfo.map((item) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="aboutWeather">
+      <h1 className="weekDey">{item.weekDey}</h1>
+      <img className="image" src={item.imgURL} />
+      <p className="temp">{item.temp}</p>
     </div>
   );
+});
+console.log(newArr);
+
+function App() {
+  return <div className="App">{newArr}</div>;
 }
 
 export default App;
